7e78bc3f181c920211169eea04a769414c5fa174
David Krauthamer <dkrautha@pm.me>
2024-03-06 14:40:20 -0500

    Use picamera2

--- coral_base.Dockerfile
+++ coral_base.Dockerfile
@@ -22,5 +22,6 @@ RUN apt-get update && apt-get install -y --no-install-recommends \
     libedgetpu1-std \
     python3-pycoral \
     python3-opencv \
+    python3-picamera2 \
     && apt-get clean \
     && rm -rf /var/lib/apt/lists/*
--- src/cats_tensorflow/__init__.py
+++ src/cats_tensorflow/__init__.py
@@ -1,8 +1,19 @@
+import time
+import cv2
 import tensorflow as tf
 
 
 def main():
-    print("TensorFlow version:", tf.__version__)
+    cap = cv2.VideoCapture("/dev/video0")
+    counter = 0
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+        counter += 1
+        if counter > 150:
+            cv2.imwrite("./frame.png", frame)
+            break
 
 
 if __name__ == "__main__":
