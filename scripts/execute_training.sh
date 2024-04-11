77a6e348890b0cd26d5fac413d16f5984d0eb1cd
David Krauthamer <dkrautha@pm.me>
2024-04-11 14:57:37 -0400

    Update training script to produce a graph output

--- scripts/execute_training.sh
+++ scripts/execute_training.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 python3 /models/research/object_detection/model_main_tf2.py \
---pipeline_config_path=/models/mymodel/pipeline_file.config \
---model_dir=/models/mymodel \
---alsologtostderr \
---num_train_steps=40000
+    --pipeline_config_path=/models/mymodel/pipeline_file.config \
+    --model_dir=/models/mymodel \
+    --alsologtostderr \
+    --num_train_steps=40000
+
+python3 /models/research/object_detection/export_tflite_graph_tf2.py \
+    --trained_checkpoint_dir=/models/mymodel/ \
+    --output_directory=/training \
+    --pipeline_config_path=/models/mymodel/pipeline_file.config
+
+
