4b72e6cf93c1d10c84aad891b7dcd4005c47d023
David Krauthamer <dkrautha@pm.me>
2024-04-11 14:58:36 -0400

    Update training script again

--- scripts/execute_training.sh
+++ scripts/execute_training.sh
@@ -6,9 +6,9 @@ python3 /models/research/object_detection/model_main_tf2.py \
     --alsologtostderr \
     --num_train_steps=40000
 
+cp -r /models/mymodel /training
+
 python3 /models/research/object_detection/export_tflite_graph_tf2.py \
     --trained_checkpoint_dir=/models/mymodel/ \
     --output_directory=/training \
     --pipeline_config_path=/models/mymodel/pipeline_file.config
-
-
